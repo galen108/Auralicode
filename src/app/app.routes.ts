6db9d9ee2c048fcbdac5c9f2ee828f4cf30ac0fe
galen108 <galen.w.tenney@gmail.com>
2024-09-15 06:33:17 -0400

    Auralicode Capabilities PNGs, Preliminary Site CSS, and App Routes Home Path Redirect Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {AboutComponent} from "./about/about.component";
 import {CapabilitiesComponent} from "./capabilities/capabilities.component";
 import {PortfolioComponent} from "./portfolio/portfolio.component";
 import {ResumeCvComponent} from "./resume-cv/resume-cv.component";
-import {HomeComponent} from "./home/home.component";
-import {AboutComponent} from "./about/about.component";
-
 
 export const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'home'},
   {path: 'home', component: HomeComponent},
   {path: 'about', component: AboutComponent},
   {path: 'capabilities', component: CapabilitiesComponent},
