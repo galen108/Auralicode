64546ce1843e9dae4bc3c6bd1d29c8bd712ed911
Galen Tenney <tenney.galen@gmail.com>
2025-08-10 10:55:44 -0400

    August 9, 2025 Significant Auralicode Update b# 6 (Follow On 2)

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {MatCard, MatCardHeader, MatCardContent, MatCardImage, MatCardFooter} from "@angular/material/card";
+// import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-portfolio',
@@ -9,7 +10,9 @@ import {MatCard, MatCardHeader, MatCardContent, MatCardImage, MatCardFooter} fro
     MatCardHeader,
     MatCardContent,
     MatCardImage,
-    MatCardFooter
+    MatCardFooter,
+    // NgForOf,
+    // NgIf
   ],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css'
