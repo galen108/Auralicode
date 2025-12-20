ffd79404652cecc4717a0ff295c4ce7f84dcbe21
Galen Tenney <tenney.galen@gmail.com>
2025-12-20 12:34:00 -0500

    December 20, 2025 Auralicode Major Overhaul Update 16  (Finito Absolut)

--- src/app/web-applications/web-applications.component.ts
+++ src/app/web-applications/web-applications.component.ts
@@ -1,19 +1,20 @@
 import { Component } from '@angular/core';
-import {MatCard, MatCardContent, MatCardFooter, MatCardHeader} from "@angular/material/card";
+import {MatCard, MatCardHeader, MatCardContent, MatCardImage, MatCardFooter} from "@angular/material/card";
 import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-web-applications',
   standalone: true,
-    imports: [
-            MatCard,
-            MatCardHeader,
-            MatCardContent,
-            MatCardImage,
-            MatCardFooter,
-            RouterLink
-        ],
+       imports: [
+                  MatCard,
+                  MatCardHeader,
+                  MatCardContent,
+                  MatCardImage,
+                  MatCardFooter,
+                  RouterLink
+                ],
   templateUrl: './web-applications.component.html',
   styleUrl: './web-applications.component.css'
 })
 export class WebApplicationsComponent {}
+
--- src/app/web-foundations/web-foundations.component.ts
+++ src/app/web-foundations/web-foundations.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
-import {MatCard, MatCardContent, MatCardFooter, MatCardHeader} from "@angular/material/card";
+import {MatCard, MatCardHeader, MatCardContent, MatCardImage, MatCardFooter} from "@angular/material/card";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-web-foundations',
   standalone: true,
-  imports: [
-              MatCard,
-              MatCardHeader,
-              MatCardContent,
-              MatCardImage,
-              MatCardFooter,
-              RouterLink
-          ],
+       imports: [
+                  MatCard,
+                  MatCardHeader,
+                  MatCardContent,
+                  MatCardImage,
+                  MatCardFooter,
+                  RouterLink
+                ],
   templateUrl: './web-foundations.component.html',
   styleUrl: './web-foundations.component.css'
 })
