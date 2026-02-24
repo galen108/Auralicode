3749fb62a8b22fca11d2f1a23816b36a85aadde3
Galen Tenney <tenney.galen@gmail.com>
2026-02-24 13:25:48 -0500

    February 24th, 2026 Auralicode origin main SSR Update Final Commit

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,3 +1,15 @@
+// Add these to the VERY TOP of main.server.ts
+(global as any).window = {};
+(global as any).document = (global as any).window.document = {
+  documentElement: { style: {} },
+  createElement: () => ({ style: {} }),
+};
+(global as any).Node = {};
+(global as any).navigator = { userAgent: '' };
+(global as any).Event = {};
+(global as any).KeyboardEvent = {};
+(global as any).MouseEvent = {};
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
