7456f6070db3f163d1fc238af011c17e8b0caf9a
Galen Tenney <tenney.galen@gmail.com>
2026-02-24 21:36:43 -0500

    February 24th, 2026 Auralicode origin main New Founder-Bio Image Update (plus enormous amount of architectural and dependency code commands and refactoring) Final Commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideAnimations } from '@angular/platform-browser/animations'; // Changed this
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideAnimations(), // Standard animations are more SSR-stable than Async in V17
+    provideClientHydration()
+  ]
 };
--- src/app/founder-bio/founder-bio.component.ts
+++ src/app/founder-bio/founder-bio.component.ts
@@ -1,20 +1,19 @@
 import { Component } from '@angular/core';
 import {MatCard, MatCardHeader, MatCardContent, MatCardImage, MatCardFooter} from "@angular/material/card";
 import {RouterLink, RouterLinkActive} from "@angular/router";
-// import {MatListItem} from "@angular/material/list";
 
 @Component({
   selector: 'app-founder-bio',
   standalone: true,
   imports: [
-    MatCard,
-    MatCardHeader,
-    MatCardContent,
-    MatCardImage,
-    MatCardFooter,
-    RouterLink,
-    RouterLinkActive
-  ],
+                MatCard,
+                MatCardHeader,
+                MatCardContent,
+                MatCardImage,
+                MatCardFooter,
+                RouterLink,
+                RouterLinkActive
+          ],
   templateUrl: './founder-bio.component.html',
   styleUrl: './founder-bio.component.css'
 })
