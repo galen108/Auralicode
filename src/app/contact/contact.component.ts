f660b981e1ec9dda624036820f62e91ac86ecaed
Galen Tenney <tenney.galen@gmail.com>
2026-04-02 19:42:04 -0400

    April 2nd, 2026 [feature/upgrade-to-v21] contact.component.html and contact.component.ts update

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, inject } from '@angular/core'; // inject is here
 import { Title, Meta } from '@angular/platform-browser'; // Title and Meta are here
-import { DomSanitizer, SafeUrl } from '@angular/platform-browser'; // DomSanitizer and SafeUrl are here
 import { MatCard, MatCardHeader, MatCardContent, MatCardFooter } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
 
@@ -20,9 +19,6 @@ export class ContactComponent implements OnInit {
   // These should be defined inside the class, but before the ngOnInit
   private titleService = inject(Title);
   private metaService = inject(Meta);
-  private sanitizer = inject(DomSanitizer);
-  // Mark the URL as trusted
-  smsLink: SafeUrl = this.sanitizer.bypassSecurityTrustUrl('sms:+13015805172');
 
   ngOnInit(): void {
     // These should work now because 'this.titleService' matches the variable above
